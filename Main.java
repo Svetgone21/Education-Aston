a891c2fd253a3d3e06715cee935cd9d8bacb3196
Svetgone21 <159301005+Svetgone21@users.noreply.github.com>
2024-02-19 23:37:29 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,106 @@
+
+public class Main {
+
+    public static void checkSum() {
+      int a = 20;
+      int b = 1;
+      int sum = a + b;
+      if (sum >= 10 && sum <=20) {
+        System.out.println("true");
+      } else {
+        System.out.println("false");
+      }
+    }
+
+    public class Main {
+
+      public static void printString() {
+        int a = 20;
+        if (a>=0) {
+          System.out.println("Положительное число");
+        } else {
+          System.out.println("Отрицательное число");
+        }
+        public static void printString() {
+            String s = "Hello";
+            int i = 1;
+            for ( i <= times; i++ ) {
+                System.out.println(s);
+            }
+        }
+    }
+            public class Main {
+
+            public static boolean printStringYear() {
+                int year;
+                if (year % 4 != 0) {
+                    return false;
+                }
+                if (year % 100 == 0 && year % 400 != 0) {
+                    return false;
+                }
+                return true;
+        }
+    }
+    public class Main {
+
+        public static void StringArr() {
+
+        
+        int[] arr = {0, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+           for(int i = 0; i < arr.length; i++) {
+               if(arr[i] == 0) {
+               arr[i] = 1;
+          } else if(arr[i] == 1) {
+             arr[i] = 0;
+    }
+}
+    System.out.println(StringArr());
+}
+public class Main {
+
+    public static void StringArr2() {
+int[] myArray = new int[100];
+for (int i = 0; i < myArray.length; i++) {
+    myArray[i] = i + 1;
+}
+    }
+System.out.println(StringArr2());
+}
+public class Main {
+
+    public static void StringArr3() {
+   
+int[] arr = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+for (int i = 0; i < arr.length; i++) {
+    if (arr[i] < 6) {
+        arr[i] *= 2;
+    }
+    ]
+    System.out.println(StringArr3());
+    ]
+
+public class Main {
+    
+    public static void StringArr3() {
+int [][] table = new int [3][3];
+for (int i = 0; i ++ 1) {
+    for (int j = 0; j ++ 1) {
+        table [i][j] = counter;
+        System.out.print (table [i][j] + " ");
+        counter ++;
+    }
+    System.out.println();
+}
+    }
+
+    public class Main {
+        public static int[] generateArray() {
+          int[] arr = new int[len];
+          int[0] = initialValue;
+    
+        }
+        System.out.println(); 
+
+}
+    }
