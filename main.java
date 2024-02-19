e7de13c858281d152a736edb29fc8943daba3fce
Svetgone21 <159301005+Svetgone21@users.noreply.github.com>
2024-02-19 20:07:23 +0600

    Update main.java

--- main.java
+++ main.java
@@ -1,24 +1,34 @@
-public class main 
-{
-   public static void printThreeWords( String [] args)
-   {
+public class Main{
+    public static void main(String [] args)
+    {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+   public static void printThreeWords(){
     System.out.println("Orange");
     System.out.println("Banana");
     System.out.println("Apple");
 
    }
-   public static void checkSumSign( int a, int b)
-   {
+
+
+   public static void checkSumSign(){
+    int a = 1;
+    int b = 3;
     int sum = a + b;
     if (sum >= 0) {
       System.out.println("Сумма положительная");
     }  else {
        System.out.println("Сумма отрицательная");
-
     }
 }
-     public void printColor(int value) 
-    {
+
+
+     public void printColor(){
+        int value = 99;
     if (value < 0) {
      System.out.println("Красный");
     } else if (value >= 0 && value <= 100) {
@@ -27,8 +37,7 @@ public class main
         System.out.println("Зеленый");
     }
 }
-  public void compareNumbers() 
-  {
+  public void compareNumbers() {
     int a = 6;
     int b = 5;
     if (a >= b) {
