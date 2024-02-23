d10217d08b2b18a400e1ef8cf4f34a53089fb995
Svetgone21 <159301005+Svetgone21@users.noreply.github.com>
2024-02-23 22:00:27 +0600

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,69 @@
+
+public class Employee
+{
+  public static void main() 
+  {
+       private String name;
+       private String position;
+       private String email;
+       private long phoneNumber;
+       private int age;
+       private floats salary;
+       public Employee (String name, String position, String email, long phoneNumber, int age, floats salary) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phoneNumber;
+        this.age = age;
+        this.salary = salary;
+       
+        {
+       name = "Иванов Иван Иванович";
+       position = "Инженер";
+       email = "ivanovii@gmail.com";
+       phoneNumber ="+79504003069";
+       age = 38;
+       salary = 78000,50;
+       }
+      }
+    System.out.println (""ФИО: "name + "; "должность: "position + ""; "почта: "email + ""; "телефон: "phoneNumber + ""; "возраст: "age + ""; "зарплата: "salary + """);
+  
+      }    
+
+}
+
+
+{
+  Person[] persArray = new Person[5];
+  persArray[0] = new Person {"Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", "30000", "30"};
+  persArray[1] = new Person {"Petrov Pavel", "Engineer", "petrpav@mailbox.com", "89213103569", "30000", "35"};
+  persArray[2] = new Person {"Dymov Pavel", "Engineer", "dymovask@mailbox.com", "89235103569", "30000", "28"};
+  persArray[3] = new Person {"Sidorov Alexandr", "Engineer", "sashasid@mailbox.com", "89250305369", "30000", "29"};
+  persArray[4] = new Person {"Zolotov Dmitry", "Engineer", "dymazolotov@mailbox.com", "89111965547", "30000", "32"};
+  int a = persArray.length;
+public static void Main () {
+  System.out.println(a);
+}
+
+
+    public class Park {
+      public static void main() {
+           private string  infoAttractions;
+           private int time;
+           private floats cost;
+           public Park (string infoAttractions,  int time, floats cost) {
+            this.infoAttractions = infoAttractions;
+            this.time = time;
+            this.cost = cost;
+            
+          
+            {
+           infoAttractions = "карусель";
+           time = "8";
+           salary = "250";
+           }
+          }
+        System.out.println ( "Информация:"infoAttractions+ "; время:"time+ "; стоимость:"cost+ " );
+      
+          }    
+        }
